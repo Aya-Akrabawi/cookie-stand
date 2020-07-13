7538f2cf7dbc745bbd843d808f7f3e04af124ca7
Your Name In Single Quotes <your email here in single quotes>
2020-07-13 17:42:49 +0300

    create table in app.js & create JSstyle.css

--- app.js
+++ app.js
@@ -1,5 +1,78 @@
 'use restrict'
-var workinghrs = ['06am', '07am', '08am', '09am', '10am', '11am', '12pm', '01pm', '02pm', '03pm', '04pm', '05pm', '06pm', '07pm']
+var hour = 6
+var hour2 = 1
+var hourarray = []
+
+for (let index = 0; index < 14 ; index++) {
+    if (index <6 ){  
+    var array = (hour+'am')
+       //console.log (array)
+       hour++ 
+    }else if (index>6){
+        var array = (hour2+'pm')
+       //console.log (array)
+       hour2++
+    } else {
+        var array = (hour+'pm')
+       //console.log (array)
+       hour++
+    }
+    hourarray.push(array)
+    
+}
+//console.log(hourarray)
+
+function headerRow() {
+    var parentElement = document.getElementById('random-hrs')
+    var table = document.createElement('table')
+    document.createAttribute('class','change')
+    table.setAttribute ('class','yala')
+    var tr = document.createElement('tr')
+    var th0 = document.createElement('th')
+        th0.textContent = ''
+        tr.append(th0)
+
+    for (var headrow = 0; headrow < hourarray.length; headrow++) {
+        var th1 = document.createElement('th')
+        th1.textContent = hourarray[headrow]
+        tr.append(th1)
+        
+    }    
+    table.appendChild(tr)
+    parentElement.appendChild(table)
+    var th2 = document.createElement('th')
+        th2.textContent = 'Daily Location Total'
+        tr.append(th2)
+
+   
+}
+
+headerRow()
+
+function FooterRow() {
+    var parentElement = document.getElementById('random-hrs')
+    var table = document.createElement('table')
+    //table.setAttribute ('border','1')
+    var tr = document.createElement('tr')
+    var th0 = document.createElement('th')
+        th0.textContent = 'Total'
+        tr.append(th0)
+
+    for (var FooterRow = 0; FooterRow < hourarray.length; FooterRow++) {
+        var th1 = document.createElement('th')
+        th1.textContent = ''
+        tr.append(th1)
+        
+    }    
+    table.appendChild(tr)
+    parentElement.appendChild(table)
+    var th2 = document.createElement('th')
+        th2.textContent = 'Daily Location Total'
+        tr.append(th2)
+
+   
+}
+    
 function Sales(name, minHourlyCustomer, maxHourlyCustomer, avgCookiesPerCustomer, hrArray, total ){
     var array1 = []
     array1.push(hrArray)
@@ -11,44 +84,52 @@ function Sales(name, minHourlyCustomer, maxHourlyCustomer, avgCookiesPerCustomer
     this.total = total
 }
 Sales.prototype.SetrandNumOfCustomersPerHr = function (){
-    for (var index = 0; index < workinghrs.length ; index++) {
+    for (var index = 0; index < hourarray.length ; index++) {
         var randomly = Math.floor(Math.random() * (this.maxHourlyCustomer - this.minHourlyCustomer + 1) + this.minHourlyCustomer);
         this.hrArray.push(Math.floor (this.avgCookiesPerCustomer) * randomly)
 
         this.total=this.total+(Math.floor (this.avgCookiesPerCustomer) * randomly)
-        console.log(this.total)
-    }
-    }
-
-    Sales.prototype.render = function() {
-        var parentElement = document.getElementById('random-hrs')
-        var article = document.createElement('article')
-        parentElement.appendChild(article)
-
-        var branch = document.createElement('strong')
-        article.appendChild(branch)
-        branch.textContent = this.name
-
-        var ul = document.createElement('ul')
-        article.appendChild(ul)
-        
-        
-        for (var hrs = 0; hrs < 14; hrs++) {
-            var li = document.createElement("li")
-            li.textContent = workinghrs[hrs] + ':   ' + this.hrArray[hrs+1] + "   cookies"
-            ul.appendChild(li)
         }
-        li.textContent = 'Total:   ' +  this.total + "   cookies"
     }
 
-    var seattle = new Sales('Seattle', 23, 65, 6.3, [], 0);
+   Sales.prototype.render = function() {
+        var parentElement = document.getElementById('random-hrs')
+        var table = document.createElement('table')
+
+       //table.setAttribute ( "style", 'border: 1px solid blue' )
+       //document.getElementById("myDiv").style.border = "thin dotted red";
+        var tr = document.createElement('tr')
+        
+        
+        
+        var th1 = document.createElement('th')
+        th1.textContent = this.name
+        tr.append(th1)
+        
+
+        for (var branchRow = 0; branchRow < hourarray.length; branchRow++) {
+            var th1 = document.createElement('th')
+            th1.textContent = ' '
+            tr.append(th1)
+            //th1.setAttribute ( "style", 'border: 1px solid blue' )
+
+        }  
+
+
+
+        table.appendChild(tr)
+        parentElement.appendChild(table)
+
+       
+    }
+   var seattle = new Sales('Seattle', 23, 65, 6.3, [], 0);
     var tokyo = new Sales('Tokyo', 23, 65, 6.3, [], 0);
     var dubai =  new Sales('Dubai', 23, 65, 6.3, [], 0);
     var paris = new Sales('Paris', 23, 65, 6.3, [], 0);
     var lima = new Sales('Lima', 23, 65, 6.3, [], 0);
 seattle.SetrandNumOfCustomersPerHr()
 seattle.render()
-/tokyo.SetrandNumOfCustomersPerHr()
+tokyo.SetrandNumOfCustomersPerHr()
 tokyo.render()
 dubai.SetrandNumOfCustomersPerHr()
 dubai.render()
@@ -56,26 +137,4 @@ paris.SetrandNumOfCustomersPerHr()
 paris.render()
 lima.SetrandNumOfCustomersPerHr()
 lima.render()
-
-
-
-/*var hour = 6
-var hour2 = 1
-var hourarray = []
-console.log(h)
-for (let index = 0; index < 14 ; index++) {
-    if (index <6 ){  
-    var array = (hour+'am')
-       console.log (array)
-       hour++ 
-    }else if (index>6){
-        var array = (hour2+'pm')
-       console.log (array)
-       hour2++
-    } else {
-        var array = (hour+'pm')
-       console.log (array)
-       hour++
-    }
-}
-console.log(array[])*/
+FooterRow()
