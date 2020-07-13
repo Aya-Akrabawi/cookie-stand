92c04c4cd12a1786584e76dd97889c99426d93a0
Your Name In Single Quotes <your email here in single quotes>
2020-07-13 14:42:53 +0300

    put the objects inside one constructor

--- app.js
+++ app.js
@@ -1,4 +1,64 @@
 'use restrict'
+var workinghrs = ['06am', '07am', '08am', '09am', '10am', '11am', '12pm', '01pm', '02pm', '03pm', '04pm', '05pm', '06pm', '07pm']
+function Sales(name, minHourlyCustomer, maxHourlyCustomer, avgCookiesPerCustomer, hrArray, total ){
+    var array1 = []
+    array1.push(hrArray)
+    this.name = name
+    this.minHourlyCustomer = minHourlyCustomer
+    this.maxHourlyCustomer = maxHourlyCustomer
+    this.avgCookiesPerCustomer = avgCookiesPerCustomer
+    this.hrArray= array1
+    this.total = total
+}
+Sales.prototype.SetrandNumOfCustomersPerHr = function (){
+    for (var index = 0; index < workinghrs.length ; index++) {
+        var randomly = Math.floor(Math.random() * (this.maxHourlyCustomer - this.minHourlyCustomer + 1) + this.minHourlyCustomer);
+        this.hrArray.push(Math.floor (this.avgCookiesPerCustomer) * randomly)
+
+        this.total=this.total+(Math.floor (this.avgCookiesPerCustomer) * randomly)
+        console.log(this.total)
+    }
+    }
+
+    Sales.prototype.render = function() {
+        var parentElement = document.getElementById('random-hrs')
+        var article = document.createElement('article')
+        parentElement.appendChild(article)
+
+        var branch = document.createElement('strong')
+        article.appendChild(branch)
+        branch.textContent = this.name
+
+        var ul = document.createElement('ul')
+        article.appendChild(ul)
+        
+        
+        for (var hrs = 0; hrs < 14; hrs++) {
+            var li = document.createElement("li")
+            li.textContent = workinghrs[hrs] + ':   ' + this.hrArray[hrs+1] + "   cookies"
+            ul.appendChild(li)
+        }
+        li.textContent = 'Total:   ' +  this.total + "   cookies"
+    }
+
+    var seattle = new Sales('Seattle', 23, 65, 6.3, [], 0);
+    var tokyo = new Sales('Tokyo', 23, 65, 6.3, [], 0);
+    var dubai =  new Sales('Dubai', 23, 65, 6.3, [], 0);
+    var paris = new Sales('Paris', 23, 65, 6.3, [], 0);
+    var lima = new Sales('Lima', 23, 65, 6.3, [], 0);
+seattle.SetrandNumOfCustomersPerHr()
+seattle.render()
+/tokyo.SetrandNumOfCustomersPerHr()
+tokyo.render()
+dubai.SetrandNumOfCustomersPerHr()
+dubai.render()
+paris.SetrandNumOfCustomersPerHr()
+paris.render()
+lima.SetrandNumOfCustomersPerHr()
+lima.render()
+
+
+
 /*var hour = 6
 var hour2 = 1
 var hourarray = []
@@ -19,217 +79,3 @@ for (let index = 0; index < 14 ; index++) {
     }
 }
 console.log(array[])*/
-var workinghrs = ['06am', '07am', '08am', '09am', '10am', '11am', '12pm', '01pm', '02pm', '03pm', '04pm', '05pm', '06pm', '07pm']
-var seattle = {
-    name: 'Seattle', 
-    minHourlyCustomer: 23,
-    maxHourlyCustomer: 65,
-    avgCookiesPerCustomer: 6.3,
-    array1: [],
-    total: 0,
-    SetrandNumOfCustomersPerHr: function () {
-    for (var index = 0; index < workinghrs.length ; index++) {
-        var randomly = Math.floor(Math.random() * (this.maxHourlyCustomer - this.minHourlyCustomer + 1) + this.minHourlyCustomer);
-        this.array1.push(Math.floor (this.avgCookiesPerCustomer) * randomly)
-        this.total=this.total+randomly
-    }
-    },
-    render: function(){
-        var parentElement = document.getElementById('random-hrs')
-        var article = document.createElement('article')
-        parentElement.appendChild(article)
-
-        var branch = document.createElement('strong')
-        article.appendChild(branch)
-        branch.textContent = this.name
-
-        var ul = document.createElement('ul')
-        article.appendChild(ul)
-        
-        
-        for (var hrs = 0; hrs < 14; hrs++) {
-            var li = document.createElement("li")
-            li.textContent = workinghrs[hrs] + ':   ' + this.array1[hrs] + "   cookies"
-            ul.appendChild(li)
-        }
-        li.textContent = 'Total:   ' +  this.total + "   cookies"
-    },
-
-
-
-}
-seattle.SetrandNumOfCustomersPerHr()
-seattle.render()
-
-var Tokyo = {
-    name: 'Tokoyo', 
-    minHourlyCustomer: 3,
-    maxHourlyCustomer: 24,
-    avgCookiesPerCustomer: 1.2,
-    array1: [],
-    total: 0,
-    SetrandNumOfCustomersPerHr: function () {
-    for (var index = 0; index < workinghrs.length ; index++) {
-        var randomly = Math.floor(Math.random() * (this.maxHourlyCustomer - this.minHourlyCustomer + 1) + this.minHourlyCustomer);
-        this.array1.push(Math.floor (this.avgCookiesPerCustomer) * randomly)
-        this.total=this.total+randomly
-    }
-    },
-    render: function(){
-        var parentElement = document.getElementById('random-hrs')
-        var article = document.createElement('article')
-        parentElement.appendChild(article)
-
-        var branch = document.createElement('strong')
-        article.appendChild(branch)
-        branch.textContent = this.name
-
-        var ul = document.createElement('ul')
-        article.appendChild(ul)
-        
-        
-        for (var hrs = 0; hrs < 14; hrs++) {
-            var li = document.createElement("li")
-            li.textContent = workinghrs[hrs] + ':   ' + this.array1[hrs] + "   cookies"
-            ul.appendChild(li)
-        }
-        li.textContent = 'Total:   ' +  this.total + "   cookies"
-    },
-
-
-
-}
-Tokyo.SetrandNumOfCustomersPerHr()
-Tokyo.render()
-
-
-
-
-var Dubai = {
-    name: 'Dubai', 
-    minHourlyCustomer: 11,
-    maxHourlyCustomer: 38,
-    avgCookiesPerCustomer: 3.7,
-    array1: [],
-    total: 0,
-    SetrandNumOfCustomersPerHr: function () {
-    for (var index = 0; index < workinghrs.length ; index++) {
-        var randomly = Math.floor(Math.random() * (this.maxHourlyCustomer - this.minHourlyCustomer + 1) + this.minHourlyCustomer);
-        this.array1.push(Math.floor (this.avgCookiesPerCustomer) * randomly)
-        this.total=this.total+randomly
-    }
-    },
-    render: function(){
-        var parentElement = document.getElementById('random-hrs')
-        var article = document.createElement('article')
-        parentElement.appendChild(article)
-
-        var branch = document.createElement('strong')
-        article.appendChild(branch)
-        branch.textContent = this.name
-
-        var ul = document.createElement('ul')
-        article.appendChild(ul)
-        
-        
-        for (var hrs = 0; hrs < 14; hrs++) {
-            var li = document.createElement("li")
-            li.textContent = workinghrs[hrs] + ':   ' + this.array1[hrs] + "   cookies"
-            ul.appendChild(li)
-        }
-        li.textContent = 'Total:   ' +  this.total + "   cookies"
-    },
-
-
-
-}
-Dubai.SetrandNumOfCustomersPerHr()
-Dubai.render()
-
-
-
-var paris = {
-    name: 'Paris', 
-    minHourlyCustomer: 20,
-    maxHourlyCustomer: 38,
-    avgCookiesPerCustomer: 2.3,
-    array1: [],
-    total: 0,
-    SetrandNumOfCustomersPerHr: function () {
-    for (var index = 0; index < workinghrs.length ; index++) {
-        var randomly = Math.floor(Math.random() * (this.maxHourlyCustomer - this.minHourlyCustomer + 1) + this.minHourlyCustomer);
-        this.array1.push(Math.floor (this.avgCookiesPerCustomer) * randomly)
-        this.total=this.total+randomly
-    }
-    },
-    render: function(){
-        var parentElement = document.getElementById('random-hrs')
-        var article = document.createElement('article')
-        parentElement.appendChild(article)
-
-        var branch = document.createElement('strong')
-        article.appendChild(branch)
-        branch.textContent = this.name
-
-        var ul = document.createElement('ul')
-        article.appendChild(ul)
-        
-        
-        for (var hrs = 0; hrs < 14; hrs++) {
-            var li = document.createElement("li")
-            li.textContent = workinghrs[hrs] + ':   ' + this.array1[hrs] + "   cookies"
-            ul.appendChild(li)
-        }
-        li.textContent = 'Total:   ' +  this.total + "   cookies"
-    },
-
-
-
-}
-paris.SetrandNumOfCustomersPerHr()
-paris.render()
-
-
-
-
-
-var Lima = {
-    name: 'Lima', 
-    minHourlyCustomer: 20,
-    maxHourlyCustomer: 38,
-    avgCookiesPerCustomer: 2.3,
-    array1: [],
-    total: 0,
-    SetrandNumOfCustomersPerHr: function () {
-    for (var index = 0; index < workinghrs.length ; index++) {
-        var randomly = Math.floor(Math.random() * (this.maxHourlyCustomer - this.minHourlyCustomer + 1) + this.minHourlyCustomer);
-        this.array1.push(Math.floor (this.avgCookiesPerCustomer) * randomly)
-        this.total=this.total+randomly
-    }
-    },
-    render: function(){
-        var parentElement = document.getElementById('random-hrs')
-        var article = document.createElement('article')
-        parentElement.appendChild(article)
-
-        var branch = document.createElement('strong')
-        article.appendChild(branch)
-        branch.textContent = this.name
-
-        var ul = document.createElement('ul')
-        article.appendChild(ul)
-        
-        
-        for (var hrs = 0; hrs < 14; hrs++) {
-            var li = document.createElement("li")
-            li.textContent = workinghrs[hrs] + ':   ' + this.array1[hrs] + "   cookies"
-            ul.appendChild(li)
-        }
-        li.textContent = 'Total:   ' +  this.total + "   cookies"
-    },
-
-
-
-}
-Lima.SetrandNumOfCustomersPerHr()
-Lima.render()
