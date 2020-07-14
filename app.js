23cfa345b244f143a2de989776327933ada6257e
Your Name In Single Quotes <your email here in single quotes>
2020-07-14 22:38:48 +0300

    add functions to show the totals

--- app.js
+++ app.js
@@ -26,15 +26,15 @@ function headerRow() {
     var parentElement = document.getElementById('random-hrs')
     var table = document.createElement('table')
     document.createAttribute('class','change')
-    table.setAttribute ('class','yala')
     var tr = document.createElement('tr')
     var th0 = document.createElement('th')
-        th0.textContent = ''
+    th0.setAttribute ( "style", 'width: 200px')
+        th0.textContent = 'Branch'
         tr.append(th0)
 
     for (var headrow = 0; headrow < hourarray.length; headrow++) {
         var th1 = document.createElement('th')
-        th1.textContent = hourarray[headrow]
+            th1.textContent = hourarray[headrow]
         tr.append(th1)
         
     }    
@@ -49,55 +49,74 @@ function headerRow() {
 
 headerRow()
 
-function FooterRow() {
+/*function footerCalc(){
+    var columnSum = []
+    for (let index = 1; index < hourarray.length+1; index++) {
+        columnSum.push(seattle.hrArray[index] + tokyo.hrArray[index] + dubai.hrArray[index] + paris.hrArray[index] + lima.hrArray[index] )
+        console.log(columnSum[12])
+    }
+}*/
+
+function RenderFooterRow() {
+    var columnSum = []
+    for (let index = 0; index < hourarray.length; index++) {
+        columnSum.push(seattle.hrArray[index] + tokyo.hrArray[index] + dubai.hrArray[index] + paris.hrArray[index] + lima.hrArray[index] )
+        
+    }
+
+    console.log(columnSum[12])
     var parentElement = document.getElementById('random-hrs')
     var table = document.createElement('table')
-    //table.setAttribute ('border','1')
     var tr = document.createElement('tr')
     var th0 = document.createElement('th')
+    th0.setAttribute ( "style", 'width: 200px')
         th0.textContent = 'Total'
         tr.append(th0)
 
     for (var FooterRow = 0; FooterRow < hourarray.length; FooterRow++) {
         var th1 = document.createElement('th')
-        th1.textContent = ''
+            th1.textContent = columnSum[FooterRow]
         tr.append(th1)
         
     }    
     table.appendChild(tr)
     parentElement.appendChild(table)
-    var th2 = document.createElement('th')
-        th2.textContent = 'Daily Location Total'
-        tr.append(th2)
+    
 
    
 }
     
-function Sales(name, minHourlyCustomer, maxHourlyCustomer, avgCookiesPerCustomer, hrArray, total ){
+function Sales(name, minHourlyCustomer, maxHourlyCustomer, avgCookiesPerCustomer, hrArray, totalperbranch, cellperhr ){
     var array1 = []
     array1.push(hrArray)
+    var array2 = []
+    array2.push(totalperbranch)
+   var array3 = []
+    array3.push(cellperhr)
     this.name = name
     this.minHourlyCustomer = minHourlyCustomer
     this.maxHourlyCustomer = maxHourlyCustomer
     this.avgCookiesPerCustomer = avgCookiesPerCustomer
     this.hrArray= array1
-    this.total = total
+    this.totalperbranch = array2
+    this.total = 0
+    this.cellperhr = array3
 }
 Sales.prototype.SetrandNumOfCustomersPerHr = function (){
     for (var index = 0; index < hourarray.length ; index++) {
         var randomly = Math.floor(Math.random() * (this.maxHourlyCustomer - this.minHourlyCustomer + 1) + this.minHourlyCustomer);
         this.hrArray.push(Math.floor (this.avgCookiesPerCustomer) * randomly)
-
         this.total=this.total+(Math.floor (this.avgCookiesPerCustomer) * randomly)
-        }
     }
+    
+    }
+
+    
 
    Sales.prototype.render = function() {
         var parentElement = document.getElementById('random-hrs')
         var table = document.createElement('table')
 
-       //table.setAttribute ( "style", 'border: 1px solid blue' )
-       //document.getElementById("myDiv").style.border = "thin dotted red";
         var tr = document.createElement('tr')
         
         
@@ -109,11 +128,16 @@ Sales.prototype.SetrandNumOfCustomersPerHr = function (){
 
         for (var branchRow = 0; branchRow < hourarray.length; branchRow++) {
             var th1 = document.createElement('th')
-            th1.textContent = ' '
-            tr.append(th1)
-            //th1.setAttribute ( "style", 'border: 1px solid blue' )
 
-        }  
+            th1.textContent = this.hrArray[branchRow+1]
+            tr.append(th1)
+
+
+        } 
+         var th1 = document.createElement('th')
+
+        th1.textContent = this.total
+        tr.append(th1)
 
 
 
@@ -122,11 +146,11 @@ Sales.prototype.SetrandNumOfCustomersPerHr = function (){
 
        
     }
-   var seattle = new Sales('Seattle', 23, 65, 6.3, [], 0);
-    var tokyo = new Sales('Tokyo', 23, 65, 6.3, [], 0);
-    var dubai =  new Sales('Dubai', 23, 65, 6.3, [], 0);
-    var paris = new Sales('Paris', 23, 65, 6.3, [], 0);
-    var lima = new Sales('Lima', 23, 65, 6.3, [], 0);
+   var seattle = new Sales('Seattle', 23, 65, 6.3);
+    var tokyo = new Sales('Tokyo', 23, 65, 6.3);
+    var dubai =  new Sales('Dubai', 23, 65, 6.3);
+    var paris = new Sales('Paris', 23, 65, 6.3);
+    var lima = new Sales('Lima', 23, 65, 6.3);
 seattle.SetrandNumOfCustomersPerHr()
 seattle.render()
 tokyo.SetrandNumOfCustomersPerHr()
@@ -137,4 +161,6 @@ paris.SetrandNumOfCustomersPerHr()
 paris.render()
 lima.SetrandNumOfCustomersPerHr()
 lima.render()
-FooterRow()
+console.log(lima.total)
+//footerCalc()
+RenderFooterRow()
